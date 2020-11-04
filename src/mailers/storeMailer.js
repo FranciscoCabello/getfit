8187579e7b5dc56f436029460ec7cff15decbd82
franciscocabello <fcabello@uc.cl>
2020-11-04 12:54:41 -0300

    mail

--- src/mailers/storeMailer.js
+++ src/mailers/storeMailer.js
@@ -1,5 +1,5 @@
 module.exports = {
   sendOwnerEmail: (ctx, data) => ctx.sendMail('sendEmailOwners', { to: data.user.email }, { data }),
-  sendGetFitEmail: (ctx, data) => ctx.sendMail('sendEmailGetFit', { to: 'getfit@gmail.com' }, { data }),
+  sendGetFitEmail: (ctx, data) => ctx.sendMail('sendEmailGetFit', { to: 'getfit123462717372@gmail.com' }, { data }),
   sendBuyerEmail: (ctx, data) => ctx.sendMail('sendEmailBuyer', { to: data.user.email }, { data }),
 };
